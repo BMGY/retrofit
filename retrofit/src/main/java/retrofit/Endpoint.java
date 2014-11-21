db18ad1877a202eaa1feb837037d478e1806f295
bmgy <bmgy@live.cn>
2014-11-21 09:27:27 +0800

    Format

--- retrofit/src/main/java/retrofit/Endpoint.java
+++ retrofit/src/main/java/retrofit/Endpoint.java
@@ -1,12 +1,13 @@
 package retrofit;
 
 /**
- * Represents an API endpoint URL and associated name. Callers should always consult the instance
- * for the latest values rather than caching the returned values.
- *
+ * Represents an API endpoint URL and associated name. Callers should always
+ * consult the instance for the latest values rather than caching the returned
+ * values.
+ * 
  * @author Matt Hickman (mhickman@palantir.com)
  */
 public interface Endpoint {
-  /** The base API URL. */
-  String getUrl();
+	/** The base API URL. */
+	String getUrl();
 }
--- retrofit/src/main/java/retrofit/mime/MimeUtil.java
+++ retrofit/src/main/java/retrofit/mime/MimeUtil.java
@@ -22,28 +22,30 @@ import retrofit.Config;
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
 public final class MimeUtil {
-  private static final Pattern CHARSET = Pattern.compile("\\Wcharset=([^\\s;]+)", CASE_INSENSITIVE);
+	private static final Pattern CHARSET = Pattern.compile("\\Wcharset=([^\\s;]+)",
+			CASE_INSENSITIVE);
 
-  /**
-   * Parse the MIME type from a {@code Content-Type} header value or default to "GBK".
-   *
-   * @deprecated Use {@link #parseCharset(String, String)}.
-   */
-  @Deprecated
-  public static String parseCharset(String mimeType) {
-    return parseCharset(mimeType, Config.getCharset());
-  }
+	/**
+	 * Parse the MIME type from a {@code Content-Type} header value or default
+	 * to "UTF-8".
+	 * 
+	 * @deprecated Use {@link #parseCharset(String, String)}.
+	 */
+	@Deprecated
+	public static String parseCharset(String mimeType) {
+		return parseCharset(mimeType, Config.getCharset());
+	}
 
-  /** Parse the MIME type from a {@code Content-Type} header value. */
-  public static String parseCharset(String mimeType, String defaultCharset) {
-    Matcher match = CHARSET.matcher(mimeType);
-    if (match.find()) {
-      return match.group(1).replaceAll("[\"\\\\]", "");
-    }
-    return defaultCharset;
-  }
+	/** Parse the MIME type from a {@code Content-Type} header value. */
+	public static String parseCharset(String mimeType, String defaultCharset) {
+		Matcher match = CHARSET.matcher(mimeType);
+		if (match.find()) {
+			return match.group(1).replaceAll("[\"\\\\]", "");
+		}
+		return defaultCharset;
+	}
 
-  private MimeUtil() {
-    // No instances.
-  }
+	private MimeUtil() {
+		// No instances.
+	}
 }
